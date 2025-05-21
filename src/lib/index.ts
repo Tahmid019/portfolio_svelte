2fc279dae4072f1a16faebb8bb61912ff59cfbc5
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-05-21 17:56:35 +0530

    upd: SEO

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,9 @@
-// Reexport your entry components here
+
+export let seo = {
+	title: 'Tahmid Choudhury | Portfolio',
+	description: 'Personal project management and portfolio website',
+	image: 'https://postimg.cc/LJyBT1p5',
+	twitter: 'SEO twitter',
+	url: 'https://tahmidchoudhury.vercel.app',
+	keywords: 'Tahmid, tahmid, Choudhury, choudhury, Tahmid Choudhury, tahmidchoudhury, TahmidChoudhury, tahmidchoudhuryportfolio, portfolio, Portfolio, portfolio-website, PortfolioWebsite, svelte, Svelte, PortfolioSvelte, sveltekit, tailind-css, typescript'
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
+	import { seo } from '$lib/index.js';
 	import About from '$lib/components/About.svelte';
 	import Hero from '$lib/components/Hero.svelte';
 	import Por from '$lib/components/Por.svelte';
 	import Projects from '$lib/components/Projects.svelte';
 	import Skills from '$lib/components/Skills.svelte';
 </script>
+
+<svelte:head>
+	<title>{seo.title}</title>
+	<meta name="description" content={seo.description} />
+	<meta name="keywords" content={seo.keywords} />
+
+	<meta property="og:title" content={seo.title} />
+	<meta property="og:description" content={seo.description} />
+	<meta property="og:image" content={seo.image} />
+	<meta property="og:site_name" content={seo.title} />
+	<meta property="og:url" content={seo.url} />
+
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:title" content={seo.title} />
+	<meta name="twitter:description" content={seo.description} />
+	<meta name="twitter:image" content={seo.image} />
+	<meta name="twitter:site" content="@tahmid019" />
+</svelte:head>
 
 <main class="bg-transparent">
 	<!-- <BgGrad />  -->
