00be85ba3651a5cad1ec6fd378a32fd2a02b8db9
Tahmid Ul Haque Choudhury <tahmidchoudhury019@gmail.com>
2025-08-25 20:52:26 +0530

    upd: projects

--- src/lib/data/projects.js
+++ src/lib/data/projects.js
@@ -6,6 +6,7 @@ import IconTickChanges from '$lib/assets/svg/published_with_changes.svg';
 import edusync from '$lib/assets/webp/lipsync2.webp';
 import imgWebp from '$lib/assets/webp/img2webp.webp';
 import cyfut_webp from '$lib/assets/webp/cyfut.webp';
+import tecno_webp from '$lib/assets/webp/technoApp.webp'
 // import coffee from '$lib/assets/webp/coffe.webp';
 
 import llmsim from '$lib/assets/llmsim2.gif';
@@ -18,6 +19,12 @@ import gnd_bg from '$lib/assets/aquarium.gif';
 import qnlp_gif from '$lib/assets/qnlp.gif';
 import eco_gif from '$lib/assets/ecomot1.gif';
 import fire_gif from '$lib/assets/fire1.gif';
+import connectedDots_gif from '$lib/assets/connectedDots.gif';
+import houseWithFlag_gif from '$lib/assets/houseWithFlag.gif';
+import s2sOut_gif from '$lib/assets/s2sOut.gif';
+import fireSpread_gif from '$lib/assets/fireSpread.gif';
+import hyperSpec_gif from '$lib/assets/hyperSpec.gif';
+import cloud_gif from '$lib/assets/cloud.gif';
 
 const items = [
 	{
@@ -56,15 +63,7 @@ const items = [
 		icon: IconTickChanges,
 		link: 'https://github.com/LipSync-Edusync/multispeaker-tts'
 	},
-	// {
-	// 	title: 'Image Format Converter',
-	// 	description:
-	// 	'Drone Based Forest Fire Detection, for rescue missions , navigating and providing safer path for hostages.',
-	// 	header: BentoSkeleton,
-	// 	headerImage: imgWebp,
-	// 	icon: IconTick,
-	// 	link: 'https://tc-imageconverter.streamlit.app/'
-	// },
+	
 	{
 		title: 'Fire Detection',
 		description:
@@ -128,6 +127,78 @@ const items = [
 		icon: IconTick,
 		link: 'http://qnlp.in/'
 	},
+	{
+		title: 'Networking',
+		description:
+			'A Hobby project purely for the love and Understanding of Networks. Building a Shared Chat and Gaming room on College LAN, for fun 😁.',
+		header: BentoSkeleton,
+		headerImage: connectedDots_gif,
+		icon: IconTick,
+		link: 'https://github.com/Tahmid019/Networking1'
+	},
+	{
+		title: 'SmartSwap25',
+		description:
+			'Incomplete. An AI based Application for finding suitable partners based on Indirect public response and behaviour.',
+		header: BentoSkeleton,
+		headerImage: cloud_gif,
+		icon: IconTick,
+		link: 'https://github.com/Tahmid019/SmartSwap25'
+	},
+	{
+		title: 'Cross Camera Identifier',
+		description:
+			'Player Matching AI detects players from videos, extracts features, matches identities across views using ResNet, cosine similarity, Hungarian algorithm.',
+		header: BentoSkeleton,
+		headerImage: houseWithFlag_gif,
+		icon: IconTick,
+		link: 'https://github.com/Tahmid019/CrossCamPlayer25'
+	},
+	{
+		title: 'Sign-2-Sign Translator',
+		description:
+			'Under Progress. A sign-to-sign end-to-end system translates sign sequences across languages, aiming to preserve gesture expressiveness naturally.',
+		header: BentoSkeleton,
+		headerImage: s2sOut_gif,
+		icon: IconTick,
+		link: 'https://github.com/Tahmid019/text2sign'
+	},
+	{
+		title: 'Forest Fire Simulation',
+		description:
+			'Under Progress. A Novel System aiming to Replicate the spread of natural forest fire via Custom Spread Algorithms.',
+		header: BentoSkeleton,
+		headerImage: fireSpread_gif,
+		icon: IconTick,
+		link: 'https://github.com/Tahmid019/ForestFireSim25'
+	},
+	{
+		title: 'Hybrid CNN Agromist',
+		description:
+			'Under Review. A hyperspectral CNN model predicts crop nutrient stress, enabling precision spraying via geotagged maps, saliency insights, and drone-based targeting.',
+		header: BentoSkeleton,
+		headerImage: hyperSpec_gif,
+		icon: IconTick,
+		link: '/'
+	},
+	{
+		title: 'Techno 2024 App',
+		description:
+			'Developed an event-management app with real-time updates, maps, and quizzes, achieving 1,000+ downloads within a week of launch.',
+		header: BentoSkeleton,
+		headerImage: tecno_webp,
+		icon: IconTick,
+		link: 'https://play.google.com/store/apps/details?id=org.tecnoesis.tecnoapp&pli=1'
+	},
+	// {
+	// 	title: 'Image Format Converter',
+	// 	description:
+	// 	'Simple Image Converter for my use',
+	// 	header: BentoSkeleton,
+	// 	headerImage: imgWebp,
+	// 	icon: IconTick,
+	// 	link: 'https://tc-imageconverter.streamlit.app/'
+	// },
 ];
 
 export default items;
